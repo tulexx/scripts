85a547040141acf1840b3e1c00d12072bc910166
tulexx <marcin.tula@gmail.com>
2018-11-15 11:58:10 +0100

    changed link_rename

--- link_rename.sh
+++ link_rename.sh
@@ -3,18 +3,22 @@
 DIR=$1
 OLD_PATTERN=$2
 NEW_PATTERN=$3
-RUN=$4
+
+if [ -z "$DIR" ] || [ -z "$OLD_PATTERN" ] || [ -z "$NEW_PATTERN" ]; then
+  echo -e "Usage:\n";
+  echo -e "./link_rename DIRECTORY OLD_PATTERN NEW_PATTERN\n";
+  exit;
+fi
 
 while read -r line
 do
-  # echo $line
   CUR_LINK_PATH="$(readlink "$line")"
   NEW_LINK_PATH="$CUR_LINK_PATH"
   NEW_LINK_PATH="${NEW_LINK_PATH/"$OLD_PATTERN"/"$NEW_PATTERN"}"
   echo "New link path:"
   echo "$NEW_LINK_PATH"
   if [[ -z "$RUN" ]]; then
-    read -n 1 -p "Continue?:" < /dev/tty char
+    read -n 1 -p "Continue? [y|N]:" < /dev/tty char
     if [[ "$char" != 'y' ]]; then
       exit 1;
     fi
